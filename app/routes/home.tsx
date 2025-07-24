eeae07a5c401eaba9a08fe312f26734165992834
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-24 14:36:24 +0630

    implement resumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVPilot" },
+    { name: "description", content: "AI powered Resume Analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.png')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your Applications and  Resume Ratings</h1>
+        <h2>Review your submissions and cheke AI-powered feedback.</h2>
+      </div>
+
+      {resumes.length > 0 && (<div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard
+            key={resume.id}
+            resume={resume} />
+        ))}
+      </div>)}
+    </section>
+  </main>;
 }
