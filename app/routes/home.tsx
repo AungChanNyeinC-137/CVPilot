53566104df1a1654b5150d44e805b6837d3e938a
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-24 15:19:11 +0630

    authentication with puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "CVPilot" },
@@ -10,12 +13,22 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore(); //usePuterStore has isLoading state
+  const location = useLocation();
+  const next = location.search.split('next=')[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/')
+  }, [auth.isAuthenticated, next])
+
   return <main className="bg-[url('/images/bg-main.png')] bg-cover">
     <Navbar />
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track your Applications and  Resume Ratings</h1>
         <h2>Review your submissions and cheke AI-powered feedback.</h2>
+
       </div>
 
       {resumes.length > 0 && (<div className="resumes-section">
